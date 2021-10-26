c38825ecd3b6256c2fbe2bd0eef77074c7cd9242
KevinSangregorio <kesangregorio@gmail.com>
2021-10-26 19:11:00 -0300

    [ADD/MOD] carpeta useContexts/form.css y login.jsx
    - Modifico form css para que se vea al 100vh el login y register
    - Agrego parte de la valdidacion del login

--- frontend/digital-booking-g4/src/components/Layout/Layout.jsx
+++ frontend/digital-booking-g4/src/components/Layout/Layout.jsx
@@ -5,6 +5,7 @@ import Home from "../Home/Home";
 import SideNav from "../SideNav/SideNav";
 
 import style from "./Layout.module.css";
+import Login from "../Forms/Login";
 
 export default function Layout(){
     return (
@@ -12,7 +13,7 @@ export default function Layout(){
         <Header></Header>
         {/* TODO: agregar ruteo */}
         <main>
-          <Home/>
+          <Login/>  
         </main>
         <Footer></Footer>
       </>
