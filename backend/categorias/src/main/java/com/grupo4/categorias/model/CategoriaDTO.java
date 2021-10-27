04dcd5744bb283b875e648c75d656a43ce18d5e5
Hernán Felipe Serna Gómez <hfserna@gmail.com>
2021-10-27 17:07:22 -0500

    [FEAT] Crear test para categoriaController
    - Se generan diversos test para el controlador de categorías
    - Se crea la clase JsonMapper en la carpeta utils

--- backend/categorias/src/main/java/com/grupo4/categorias/model/CategoriaDTO.java
+++ backend/categorias/src/main/java/com/grupo4/categorias/model/CategoriaDTO.java
@@ -1,12 +1,14 @@
 package com.grupo4.categorias.model;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
+@Builder
 public class CategoriaDTO {
     private Long id;
     private String titulo;
