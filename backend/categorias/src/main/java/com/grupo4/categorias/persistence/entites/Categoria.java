94ea02826022a57c6431d620e58288718acd33bc
Valentina Varela Rodríguez <vavarela.rodriguez@gmail.com>
2021-10-28 20:26:25 -0300

    [DELETE] Dependencia de Lombok
    Agregué los constructores, getters y setters necesarios.

--- backend/categorias/src/main/java/com/grupo4/categorias/persistence/entites/Categoria.java
+++ backend/categorias/src/main/java/com/grupo4/categorias/persistence/entites/Categoria.java
@@ -1,14 +1,11 @@
 package com.grupo4.categorias.persistence.entites;
 
-import lombok.Getter;
-import lombok.Setter;
-
 import javax.persistence.*;
 
 @Entity
-@Getter @Setter
 @Table(name = "categorias")
 public class Categoria {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name="categoria_id")
@@ -17,4 +14,36 @@ public class Categoria {
     private String descripcion;
     @Column(name="url_imagen")
     private String urlImagen;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public String getUrlImagen() {
+        return urlImagen;
+    }
+
+    public void setUrlImagen(String urlImagen) {
+        this.urlImagen = urlImagen;
+    }
 }
