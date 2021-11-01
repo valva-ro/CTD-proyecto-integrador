c188c7d15e0904dc4bfe861b152cdf7dba30113a
miguelEspinosa1 <espinosaorganista1@gmail.com>
2021-11-01 13:05:16 -0500

    corrección primera de carries

--- frontend/digital-booking-g4/src/components/Searcher/Searcher.jsx
+++ frontend/digital-booking-g4/src/components/Searcher/Searcher.jsx
@@ -3,8 +3,11 @@ import { useState, useEffect, useRef } from "react";
 import styles from "./Searcher.module.css";
 import FilledButton from "../Buttons/FilledButton";
 import DatePicker, { registerLocale } from "react-datepicker";
+import cities from "./cities.json"
 import es from "date-fns/locale/es";
 import "react-datepicker/dist/react-datepicker.css";
+
+
 registerLocale("es", es);
 
 export default function Searcher() {
@@ -13,6 +16,7 @@ export default function Searcher() {
   const [quantityMonth, setQuantityMonth] = useState(2);
   const [iconGps, setIconGps] = useState(styles.gps);
   const [iconDate, setIconDate] = useState(styles.dateIcon);
+  const [cityList, setCitylist] = useState(null)
   let datePickerRef = useRef(null);
 
   useEffect(() => {
@@ -28,6 +32,7 @@ export default function Searcher() {
   };
 
   const styleChange = (input) => {
+
     if (!input.target.value == "") {
       setIconDate(styles.dateIconEmpty);
     } else {
@@ -44,14 +49,57 @@ export default function Searcher() {
   };
 
   const onType = (input) => {
+
     if (!input.target.value == "") {
       setIconGps(styles.gpsEmpty);
     } else {
       setIconGps(styles.gps);
     }
   };
+  const lister = (input) => {
+    const validate = (frase) => {
+      if (frase.toLowerCase().normalize("NFD").replace(/[\u0300-\u036f]/g, "")
+        .includes((input.target.value).toLowerCase().normalize("NFD").replace(/[\u0300-\u036f]/g, ""))
+        && input.target.value != "") {
+        return true
+      } else {
+        return false
+      }
+    }
+    const newLocations = cities.filter(location =>
+      validate(location.country)
+      || validate(location.city)
+      || validate(location.city + " " + location.country)
+      || validate(location.country + " " + location.city)
+    )
+    const setInput= (input,city) =>{
+      input.target.value=city.city+", "+city.country
+      setCitylist(null)
+    }
+    if (newLocations.length > 0) {
+      setCitylist(<ul className={styles.cities} onBlur={()=>setCitylist(null)}>
+        {newLocations.map((city, index) => {
+          return <li key={index} onClick={()=> setInput(input,city)}>
 
-  return (
+            <i class="fas fa-map-marker-alt"></i>
+            <div className={styles.searchWords}>
+              <p className={styles.city}>{city.city}</p >
+              <p className={styles.country}>{city.country}</p>
+            </div>
+          </li>
+        })}
+      </ul>)
+    
+
+    } else {
+      setCitylist(null)
+     
+    }
+
+
+  }
+
+  return (<div className={styles.globalContainer}>
     <div className={styles.boxing}>
       <h2 className={styles.letter}>
         Busca ofertas en hoteles, casas y mucho más
@@ -63,21 +111,26 @@ export default function Searcher() {
           </span>
           <input
             placeholder="¿A dónde vamos?"
-            type="search"
+            type="text"
+            onblur={()=> console.log("123")}
             className={styles.input}
-            onKeyUp={(e) => onType(e)}
+            onKeyUp={(e) => lister(e, onType(e))}
+            
           />
+
+
         </div>
 
         <div className={styles.dateContainer}>
           <span className={iconDate}>
-            <i class="far fa-calendar-alt"></i>
+            <i className="far fa-calendar-alt"></i>
           </span>
+          {cityList}
         </div>
         <DatePicker
           onSelect={(e) => styleChangeClick(e)}
           onChangeRaw={(e) => styleChange(e)}
-          dateFormat="dd/MM/yyyy"
+          dateFormat="dd 'de' MMM."
           placeholderText="Check in  -  Check out"
           locale={es}
           selectsRange={true}
@@ -95,7 +148,11 @@ export default function Searcher() {
           </div>
         </DatePicker>
         <FilledButton>Buscar</FilledButton>
+
       </div>
+
     </div>
+
+  </div>
   );
 }
