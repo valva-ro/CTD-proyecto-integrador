f9f38203ed3563267adf4239da2234aef59b14bc
SofiaTohmeGarnero <sofiatohmegarnero@gmail.com>
2021-11-02 16:51:15 -0300

    [ADD] opacidad y mejora en el ruteo. Agrego opacidad en pagina del fondo al abrir el sidenav. Al clickear los botones de redireccion del sidenav este se cierre.

--- frontend/digital-booking-g4/src/components/SideNav/Options.jsx
+++ frontend/digital-booking-g4/src/components/SideNav/Options.jsx
@@ -2,6 +2,6 @@ import React from "react"
 
 export default function Options(props){
     return(
-        <p>{props.contenido}</p>
+        <p onClick={props.onClick}>{props.contenido}</p>
     )
 }
