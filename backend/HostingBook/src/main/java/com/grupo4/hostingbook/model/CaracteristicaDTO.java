f1b6207ec28a9d23b9274e3f23ccab1ed0e5d567
miguelEspinosa1 <espinosaorganista1@gmail.com>
2021-11-02 19:24:29 -0500

    Agregación entity y DTO

--- backend/HostingBook/src/main/java/com/grupo4/hostingbook/model/CaracteristicaDTO.java
+++ backend/HostingBook/src/main/java/com/grupo4/hostingbook/model/CaracteristicaDTO.java
@@ -1,4 +1,62 @@
 package com.grupo4.hostingbook.model;
 
+import javax.persistence.*;
+import java.util.Objects;
+
+
 public class CaracteristicaDTO {
+
+    private Long id;
+    private String nombre;
+    private String icono;
+
+    public CaracteristicaDTO() {}
+
+    public CaracteristicaDTO( String nombre, String icono) {
+        this.nombre = nombre;
+        this.icono = icono;
+    }
+
+    public CaracteristicaDTO(Long id, String nombre, String icono) {
+        this.id = id;
+        this.nombre = nombre;
+        this.icono = icono;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getIcono() {
+        return icono;
+    }
+
+    public void setIcono(String icono) {
+        this.icono = icono;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof CaracteristicaDTO)) return false;
+        CaracteristicaDTO that = (CaracteristicaDTO) o;
+        return Objects.equals(getId(), that.getId()) && Objects.equals(getNombre(), that.getNombre()) && Objects.equals(getIcono(), that.getIcono());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getId(), getNombre(), getIcono());
+    }
 }
