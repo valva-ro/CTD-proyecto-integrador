3cdafcf1957e341f0a61a75428736a5d26471e74
Valentina Varela Rodríguez <vavarela.rodriguez@gmail.com>
2021-11-03 13:04:56 -0300

    [FIX] Cambia el título bloque según la categoría

--- frontend/digital-booking-g4/src/components/BloqueAlojamientos/BloqueAlojamientos.jsx
+++ frontend/digital-booking-g4/src/components/BloqueAlojamientos/BloqueAlojamientos.jsx
@@ -14,22 +14,27 @@ export default function BloqueAlojamientos({ categoriaActual }) {
         );
   return (
     <section className={styles.recomendaciones}>
-      <TituloBloque>Recomendaciones</TituloBloque>
+      <TituloBloque>
+        {categoriaActual === "" ? "Recomendaciones" : capitalizeFirstLetter(categoriaActual)}
+      </TituloBloque>
       <ul className={styles.alojamientos}>
-        {alojamientosFiltrados
-          .map((alojamiento, i) => (
-            <li key={i} className={styles.alojamiento}>
-              <TarjetaAlojamiento
-                titulo={alojamiento.title}
-                imagen={alojamiento.img}
-                categoria={alojamiento.category}
-                descripcion={alojamiento.description}
-                ubicacion={alojamiento.location}
-                cantEstrellas={alojamiento.stars}
-              />
-            </li>
-          ))}
+        {alojamientosFiltrados.map((alojamiento, i) => (
+          <li key={i} className={styles.alojamiento}>
+            <TarjetaAlojamiento
+              titulo={alojamiento.title}
+              imagen={alojamiento.img}
+              categoria={alojamiento.category}
+              descripcion={alojamiento.description}
+              ubicacion={alojamiento.location}
+              cantEstrellas={alojamiento.stars}
+            />
+          </li>
+        ))}
       </ul>
     </section>
   );
+}
+
+function capitalizeFirstLetter(string) {
+  return string.charAt(0).toUpperCase() + string.slice(1);
 }
