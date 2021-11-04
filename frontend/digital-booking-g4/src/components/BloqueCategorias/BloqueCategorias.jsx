32dc253b1a4502b9a2f47c22c58d243ffa95052f
miguelEspinosa1 <espinosaorganista1@gmail.com>
2021-11-04 17:45:23 -0500

    [fix] cambio de manejo de bug

--- frontend/digital-booking-g4/src/components/BloqueCategorias/BloqueCategorias.jsx
+++ frontend/digital-booking-g4/src/components/BloqueCategorias/BloqueCategorias.jsx
@@ -9,31 +9,40 @@ export default function BloqueCategorias() {
 
   const [datos, setDatos] = useState([])
   const data = useFetch("categorias")
-  
-  
-  
+
+
+
   useEffect(() => {
-    console.log(data);
-  setDatos(data.items) 
- if (data.isLoaded) {
+
+    setDatos(data.items)
+    if (data.isLoaded) {
       setDatos(data.items)
-    }else{
-      console.log("error al cargar las categorias de la base de datos code"+data.error);
-      err.BloqueCategoriaserr[0].descripcion+=data.error
+    } else {
+      
+
       setDatos(err.BloqueCategoriaserr)
+      
     }
- 
+
 
   })
-  return (
-    <section className={styles.bloqueCategorias}>
-      <TituloBloque>Buscar por tipo de alojamiento</TituloBloque>
-      <div className={styles.listadoTarjetas}>
-        {
-          datos.map((dato, i) =>
-            <TarjetaCategoria key={`categoria-${i}`} fotoPortada={dato.urlImagen} nombre={dato.titulo} descripcion={dato.descripcion} />
-          )
-        }
-      </div>
-    </section>)
+  if (data.isLoaded) {
+    return (
+      <section className={styles.bloqueCategorias}>
+        <TituloBloque>Buscar por tipo de alojamiento</TituloBloque>
+        <div className={styles.listadoTarjetas}>
+          {
+            datos.map((dato, i) =>
+              <TarjetaCategoria key={`categoria-${i}`} fotoPortada={dato.urlImagen} nombre={dato.titulo} descripcion={dato.descripcion} />
+            )
+          }
+        </div>
+      </section>)
+  } else {
+    return (
+    <h2 className={styles.sinResultados}>Error al cargar las categorias</h2>
+            
+    )
+  }
+  
 }
--- frontend/digital-booking-g4/src/hooks/useFetch.js
+++ frontend/digital-booking-g4/src/hooks/useFetch.js
@@ -1,33 +1,40 @@
-import { useState, useEffect } from "react";
+import {
+    useState,
+    useEffect
+} from "react";
 
 
 export default function useFetch(path) {
     let [items, setItems] = useState(null);
     let [isLoaded, setIsLoaded] = useState(false);
-    let [error, setError]= useState(null)
+    let [error, setError] = useState(null)
 
     // algo parecido a un DidMount...
     useEffect(() => {
-    
+
         async function fetchData() {
 
-            try{
+            try {
+
+                let response = await fetch(`http://localhost:8080/${path}`);
+
+                let datos = await response.json();
                 
-            let response = await fetch(`http://localhost:8080/${path}`);
-            
-            let datos = await response.json();
-           console.log(datos);
-            setItems(datos)
-            setIsLoaded(true)}
-            catch (err){
+                setItems(datos)
+                setIsLoaded(true)
+            } catch (err) {
                 setError(err)
             }
         }
 
-        
-            fetchData()
-       
+
+        fetchData()
+
     }, [])
 
-    return {items, isLoaded,error }
+    return {
+        items,
+        isLoaded,
+        error
+    }
 }
