09070a0a8b3f58e7e8de47027133167de71002f1
Valentina Varela Rodríguez <vavarela.rodriguez@gmail.com>
2021-11-08 23:47:49 -0300

    [ADD] Loop infinito en desktop

--- frontend/digital-booking-g4/src/components/ProductoModalCarousel/ProductoModalCarousel.jsx
+++ frontend/digital-booking-g4/src/components/ProductoModalCarousel/ProductoModalCarousel.jsx
@@ -21,6 +21,7 @@ export default function ProductoModalCarousel({
           X
         </span>
         <Carousel
+          infiniteLoop={true}
           dynamicHeight={true}
           showArrows={true}
           showStatus={false}
