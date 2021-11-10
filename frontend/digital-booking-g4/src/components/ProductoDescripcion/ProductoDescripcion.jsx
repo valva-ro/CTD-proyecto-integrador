7c111e26c417d0d162df35ebccf0ab30bbc6164c
miguelEspinosa1 <espinosaorganista1@gmail.com>
2021-11-10 16:07:09 -0500

    [ADD] manejo de datos nulos]

--- frontend/digital-booking-g4/src/components/ProductoDescripcion/ProductoDescripcion.jsx
+++ frontend/digital-booking-g4/src/components/ProductoDescripcion/ProductoDescripcion.jsx
@@ -3,17 +3,20 @@ import styles from "./ProductoDescripcion.module.css";
 import useScreenWidth from "../../hooks/useScreenWidth";
 
 export default function ProductoDescripcion({ alojamiento }) {
-
-    return (
-        <section className={styles.bloqueProductoDescripcion}>
-            <>
-                <TituloBloque> {alojamiento.product.title} </TituloBloque>
-                <div className={styles.bloqueProductoInfo}>
-                    {alojamiento.product.info.map((parrafo,i) => {
-                        return <p key={i}>{parrafo}</p>
-                    })}
-                </div>
-            </>
-        </section>
-    );
+  return (
+    <section className={styles.bloqueProductoDescripcion}>
+      <>
+        <TituloBloque> {alojamiento.product.title} </TituloBloque>
+        <div className={styles.bloqueProductoInfo}>
+          {alojamiento.product.info.length > 0 ? (
+            alojamiento.product.info.map((parrafo, i) => {
+              return <p key={i}>{parrafo}</p>;
+            })
+          ) : (
+            <h2>No hay informacion del producto</h2>
+          )}
+        </div>
+      </>
+    </section>
+  );
 }
