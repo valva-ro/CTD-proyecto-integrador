25eaea4d20ddb9a6b35966a80f3db632e1744860
KevinSangregorio <kesangregorio@gmail.com>
2021-11-10 16:03:59 -0300

    [ADD] consumo de API producto en los componentes
    - Actualizo dependencias
    - Se quita partes harcodeadas para reemplazar por la API

--- frontend/digital-booking-g4/src/components/Producto/Producto.jsx
+++ frontend/digital-booking-g4/src/components/Producto/Producto.jsx
@@ -1,36 +1,36 @@
+import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import ProductoHeader from "../ProductoHeader/ProductoHeader";
 import ProductoImagenes from "../ProductoImagenes/ProductoImagenes.jsx";
 import ProductoFechasDisponibles from "../ProductoFechasDisponibles/ProductoFechasDisponibles";
 import ProductoMapa from "../ProductoMapa/ProductoMapa";
 import styles from "./Producto.module.css";
-import alojamientos from "../../resources/alojamientos.json";
 import useFetch from "../../hooks/useFetch";
 
 export default function Producto() {
   const { id } = useParams();
   const { isLoaded, items } = useFetch(`productos/${id}`);
-  // Para obtener el ID de la url
-  console.log("ID: "+id);
-  console.log("ISLoaded: "+isLoaded);
-  console.log("Items: "+items);
-  // TODO: cambiar esto por el GET a la API
-  const obtenerAlojamiento = () => {
-    if (id < alojamientos.length+1) return alojamientos[id - 1];
-  };
+  const [producto, setProducto] = useState(null);
+
+  useEffect(() => {
+    if (isLoaded) {
+      setProducto(items);
+    }
+  }, [isLoaded, items]);
+
   return (
     <>
-      {id >= alojamientos.length + 1 ? (
+      {isLoaded && producto !== null ? (
+        <>
+          <ProductoHeader alojamiento={producto} />
+          <ProductoImagenes alojamiento={producto} />
+          <ProductoFechasDisponibles />
+          <ProductoMapa alojamiento={producto} />
+        </>
+      ) : (
         <h2 className={styles.sinResultados}>
           El alojamiento que estás buscando no existe
         </h2>
-      ) : (
-        <>
-          <ProductoHeader alojamiento={obtenerAlojamiento()} />
-          <ProductoImagenes alojamiento={obtenerAlojamiento()} />
-          <ProductoFechasDisponibles />
-          <ProductoMapa alojamiento={obtenerAlojamiento()} />
-        </>
       )}
     </>
   );
