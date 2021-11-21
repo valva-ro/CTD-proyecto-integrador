677fde84fbf4bbe8fbe34083bcbc1ea0a88781f3
SofiaTohmeGarnero <sofiatohmegarnero@gmail.com>
2021-11-21 13:11:26 -0300

    [ADD] datepicker deshabilitar dias ant. a la fecha

--- frontend/digital-booking-g4/src/components/Producto/ProductoReserva/ProductoFechaReserva/ProductoFechaReserva.jsx
+++ frontend/digital-booking-g4/src/components/Producto/ProductoReserva/ProductoFechaReserva/ProductoFechaReserva.jsx
@@ -14,23 +14,23 @@ export default function ProductoFechaReserva( {setCheckin, setCheckout}){
 
     const [dateRange, setDateRange] = useState([null, null]);
     const [startDate, endDate] = dateRange;
-    const excludeDates = [
+    
+    /* desabilita los días anteriores al día actual*/
+    let hoy = new Date();
+    let ayer = hoy.setDate(hoy.getDate() - 1);
+    let diaDeAyer = new Date(ayer).getDate();
+    let mes = new Date(ayer).getMonth();
+    let anio = new Date(ayer).getFullYear();
+    
+    let diasAnterioresALaFecha = [];
+
+    for (let i=1; i <= diaDeAyer ; i++){
+        diasAnterioresALaFecha.push(new Date ( anio, mes, i));
+    }
+
+    let fechasReservadas = [  
         //año, mes, dia --> el mes está corrido. ej: 11 = diciembre / 12 = enero
-        new Date(2021, 2, 12),
-        new Date(2021, 10, 5),
-        new Date(2021, 10, 4),
-        new Date(2021, 10, 9),
-        new Date(2021, 10, 12),
-        new Date(2021, 10, 17),
-        new Date(2021, 10, 18),
-        new Date(2021, 10, 19),
-        new Date(2021, 10, 20),
-        new Date(2021, 10, 21),
-        new Date(2021, 10, 28),
-        new Date(2021, 10, 29),
-        new Date(2021, 10, 5),
-        new Date(2021, 10, 4),
-        new Date(2021, 10, 9),
+        //TODO: consumir de la API reservas
         new Date(2021, 11, 2),
         new Date(2021, 11, 3),
         new Date(2021, 11, 4),
@@ -42,8 +42,24 @@ export default function ProductoFechaReserva( {setCheckin, setCheckout}){
         new Date(2021, 11, 21),
         new Date(2021, 11, 28),
         new Date(2021, 11, 29),
+        new Date(2022, 0, 6),
+        new Date(2022, 0, 7),
+        new Date(2022, 0, 8),
+        new Date(2022, 0, 9),
+        new Date(2022, 0, 12),
+        new Date(2022, 0, 13),
+        new Date(2022, 0, 14),
+        new Date(2022, 0, 15),
+        new Date(2022, 0, 30),
+        new Date(2022, 0, 31),
+        new Date(2022, 1, 5),
+        new Date(2022, 1, 6),
+        new Date(2022, 1, 10),
+        new Date(2022, 1, 11),
     ];
-    
+
+    const excludeDates = diasAnterioresALaFecha.concat(fechasReservadas);
+
     const anchoPantalla = useScreenWidth();
 
     const MyContainer = ({ className, children }) => {
@@ -72,6 +88,7 @@ export default function ProductoFechaReserva( {setCheckin, setCheckout}){
                 minDate={new Date()}
                 dateFormat="dd/mm/yyyy"
                 onChange={(update) => {
+
                     setDateRange(update);
                 }}
                 selectsRange={true}
