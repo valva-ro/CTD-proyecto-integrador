003bedd402b3502ad02309f6af934221fdfabb7b
Valentina Varela Rodríguez <vavarela.rodriguez@gmail.com>
2021-11-23 21:33:57 -0300

    [FIX] Estilos del header para que sea consistente con los otros

--- frontend/digital-booking-g4/src/components/Favoritos/Favoritos.jsx
+++ frontend/digital-booking-g4/src/components/Favoritos/Favoritos.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect, useContext } from "react";
-import { Link, Redirect } from "react-router-dom";
+import { Link, Redirect, useHistory } from "react-router-dom";
 import TituloBloque from "../TituloBloque/TituloBloque";
 import TarjetaAlojamiento from "../BloqueAlojamientos/TarjetaAlojamiento/TarjetaAlojamiento";
 import useFetch from "../../hooks/useFetch";
@@ -7,6 +7,7 @@ import loggedContext from "../../contexts/loggedContext";
 import styles from "./Favoritos.module.css";
 
 export default function Favoritos() {
+  const history = useHistory();
   const idUsuario = parseInt(localStorage.getItem("id"));
   const [favoritos, setFavoritos] = useState([]);
   const { isLoaded, items } = useFetch(`usuarios/${idUsuario}`);
@@ -18,18 +19,24 @@ export default function Favoritos() {
     }
   }, [isLoaded, isLogged, items]);
 
-
   if (!isLogged) {
     return <Redirect to="/" />;
   }
 
   return (
     <>
-      <div className={styles.titleFavContainer}>
-        <TituloBloque styles={styles.favTitle}>Tus favoritos</TituloBloque>
-        <Link to="/">
-          <i className="fas fa-chevron-left"></i>
-        </Link>
+      <div className={styles.contenedorHeader}>
+        <div className={styles.headerIzquierda}>
+          <div className={styles.a} onClick={history.goBack}>
+            <span>
+              <i className="fas fa-chevron-left" aria-hidden="true"></i>
+            </span>
+            <span>ATRÁS</span>
+          </div>
+        </div>
+        <div className={styles.headerDerecha}>
+          <TituloBloque styles={styles.favTitle}>Tus favoritos</TituloBloque>
+        </div>
       </div>
       <section className={styles.favoritosContainer}>
         {favoritos.length === 0 ? (
