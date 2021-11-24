8c6cf4396de13738941bb8ff6f136e6375ca98a6
Valentina Varela Rodríguez <vavarela.rodriguez@gmail.com>
2021-11-23 23:11:40 -0300

    [FIX] Mostrar favoritos con puntuación

---
+++ backend/HostingBook/src/main/java/com/grupo4/hostingbook/service/IPuntuacionService.java
@@ -0,0 +1,9 @@
+package com.grupo4.hostingbook.service;
+
+import com.grupo4.hostingbook.model.PuntuacionDTO;
+
+import java.util.Set;
+
+public interface IPuntuacionService extends CRUDService<PuntuacionDTO>{
+    Set<PuntuacionDTO> consultarPorProductoID(Long id);
+}
