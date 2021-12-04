6680d17fe3be0111202b2dc9ec1445c6c31891c0
Valentina Varela Rodríguez <vavarela.rodriguez@gmail.com>
2021-12-04 15:49:50 -0300

    [DELETE] Metodo para validar el token

--- backend/HostingBook/src/main/java/com/grupo4/hostingbook/controller/impl/UsuarioDetailsController.java
+++ backend/HostingBook/src/main/java/com/grupo4/hostingbook/controller/impl/UsuarioDetailsController.java
@@ -2,7 +2,6 @@ package com.grupo4.hostingbook.controller.impl;
 
 import com.grupo4.hostingbook.auth.AuthenticationRequest;
 import com.grupo4.hostingbook.auth.AuthenticationResponse;
-import com.grupo4.hostingbook.exceptions.GlobalExceptionsHandler;
 import com.grupo4.hostingbook.exceptions.Mensajes;
 import com.grupo4.hostingbook.model.UsuarioDTO;
 import com.grupo4.hostingbook.service.impl.UsuarioDetailsService;
@@ -11,9 +10,7 @@ import com.grupo4.hostingbook.utils.JWTUtil;
 import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiResponse;
 import io.swagger.annotations.ApiResponses;
-import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.BadCredentialsException;
@@ -22,9 +19,7 @@ import org.springframework.security.core.Authentication;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.*;
 
-import javax.servlet.http.HttpServletRequest;
 import java.util.ArrayList;
-import java.util.Date;
 
 @RestController
 @CrossOrigin(origins = "*", methods = {RequestMethod.GET, RequestMethod.POST}, allowedHeaders = "*")
@@ -65,22 +60,5 @@ public class UsuarioDetailsController {
         final UsuarioDTO usuario = usuarioService.obtenerPorEmail(authenticationRequest.getMail());
         final String jwt = jwtTokenUtil.generateToken(userDetails);
         return ResponseEntity.ok(new AuthenticationResponse(jwt, usuario));
-    }
-
-    @ApiOperation(value = "Verifica token de un usuario")
-    @ApiResponses(value = {
-            @ApiResponse(code = 200, message = "Current session is valid"),
-            @ApiResponse(code = 401, message = "Current session has expired"),
-    })
-    @GetMapping("/validateCurrentSession")
-    public ResponseEntity<?> verificarToken(HttpServletRequest request) {
-        String token = request.getHeader("Authorization");
-        Date expiracion = jwtTokenUtil.extractExpiration(token);
-        Date ahora = new Date();
-        if (expiracion.after(ahora)) {
-            return ResponseEntity.status(HttpStatus.OK).body("La sesión es válida");
-        } else {
-            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("La sesión expiró");
-        }
     }
 }
