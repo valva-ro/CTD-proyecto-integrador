23408413eb584614288346747747cbaa27c52522
Valentina Varela Rodríguez <vavarela.rodriguez@gmail.com>
2021-12-06 18:05:47 -0300

    [FIX] Mostrar más
    No se mostraban todos los comentarios cuando la cantidad no era
    multilplo de 3

--- frontend/digital-booking-g4/src/components/Producto/ProductoDetalle/ProductoComentarios/ProductoComentarios.jsx
+++ frontend/digital-booking-g4/src/components/Producto/ProductoDetalle/ProductoComentarios/ProductoComentarios.jsx
@@ -31,7 +31,7 @@ export default function ProductoComentarios({ alojamiento: { id } }) {
   useEffect(() => {
     if (isLoaded) {
       setCargarMasComentarioDeshabilitado(
-        puntuaciones.length < cantidadComentariosActual + CANTIDAD_COMENTARIOS
+        puntuaciones.length < cantidadComentariosActual
       );
       setCargarMenosComentarioDeshabilitado(
         cantidadComentariosActual < CANTIDAD_COMENTARIOS * 2
@@ -64,22 +64,29 @@ export default function ProductoComentarios({ alojamiento: { id } }) {
         {isLoaded && puntuaciones.length === 0 ? (
           <h3 className={styles.sinResultados}>Todavía no hay comentarios</h3>
         ) : isLoaded ? (
-          puntuaciones.slice(0, cantidadComentariosActual).map((puntuacion) => (
-            <div className={styles.comentarioContainer} key={puntuacion.id}>
-              <div className={styles.estrellasContainer}>
-                <Estrellas puntaje={puntuacion.puntuacion} />
-                <p className={styles.fecha}>
-                  {formatearFecha(puntuacion.fecha)}
-                </p>
+          puntuaciones
+            .slice(
+              0,
+              puntuaciones.length < cantidadComentariosActual
+                ? puntuaciones.length
+                : cantidadComentariosActual
+            )
+            .map((puntuacion) => (
+              <div className={styles.comentarioContainer} key={puntuacion.id}>
+                <div className={styles.estrellasContainer}>
+                  <Estrellas puntaje={puntuacion.puntuacion} />
+                  <p className={styles.fecha}>
+                    {formatearFecha(puntuacion.fecha)}
+                  </p>
+                </div>
+                <h3 className={styles.nombreComentario}>
+                  {`${puntuacion.usuario.nombre} ${puntuacion.usuario.apellido}`}
+                </h3>
+                <p
+                  className={styles.comentario}
+                >{`"${puntuacion.comentario}"`}</p>
               </div>
-              <h3 className={styles.nombreComentario}>
-                {`${puntuacion.usuario.nombre} ${puntuacion.usuario.apellido}`}
-              </h3>
-              <p
-                className={styles.comentario}
-              >{`"${puntuacion.comentario}"`}</p>
-            </div>
-          ))
+            ))
         ) : null}
       </div>
       {isLoaded && puntuaciones.length > 0 ? (
