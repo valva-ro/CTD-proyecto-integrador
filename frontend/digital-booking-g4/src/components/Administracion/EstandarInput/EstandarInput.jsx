fe46a341b2b40a932b254fc67b660f2a33cf65e0
Valentina Varela Rodríguez <vavarela.rodriguez@gmail.com>
2021-12-06 21:47:46 -0300

    VERSION INCOMPLETA, NO VOLVER ACÁ

--- frontend/digital-booking-g4/src/components/Administracion/EstandarInput/EstandarInput.jsx
+++ frontend/digital-booking-g4/src/components/Administracion/EstandarInput/EstandarInput.jsx
@@ -1,31 +1,30 @@
-import styles from "./EstandarInput.module.css"
+import styles from "./EstandarInput.module.css";
 
 export default function EstandarInput({
-    label,
-    name,
-    onChangeItem,
-    setOnChangeItem,
-    placeholder,
-    id,
-    dataId
-}){
-    return(
-        <div className={styles.containerEstandarInput}>
-            <label className={styles.labelEstandarInput}>{label}</label>
-            <input
-                type="text"
-                className={`${styles.estandarInput}`}
-                onChange={(e) => {
-                    setOnChangeItem(e.target.value)
-                }}
-                value={onChangeItem}
-                name={name}
-                placeholder={placeholder}
-                id={id}
-                dataId={dataId}
-                required
-
-            />
-        </div>
-    )
+  label,
+  name,
+  onChangeItem,
+  setOnChangeItem,
+  placeholder,
+  id,
+  dataId,
+}) {
+  return (
+    <div className={styles.containerEstandarInput}>
+      <label className={styles.labelEstandarInput}>{label}</label>
+      <input
+        type="text"
+        className={`${styles.estandarInput}`}
+        onChange={(e) => {
+          setOnChangeItem(e.target.value);
+        }}
+        value={onChangeItem}
+        name={name}
+        placeholder={placeholder}
+        id={id}
+        dataId={dataId}
+        required
+      />
+    </div>
+  );
 }
--- frontend/digital-booking-g4/src/components/Administracion/RowImagenes/RowImagenes.jsx
+++ frontend/digital-booking-g4/src/components/Administracion/RowImagenes/RowImagenes.jsx
@@ -1,49 +1,98 @@
-import { useEffect } from "react";
-import DropInput from "../DropInput/DropInput"
+import { useState } from "react";
+import DropInput from "../DropInput/DropInput";
 import EstandarInput from "../EstandarInput/EstandarInput";
-import styles from "./RowImagenes.module.css"
+import styles from "./RowImagenes.module.css";
 
 export default function RowImagenes({
-    imagenesDetails,
-    handleAdd,
-    handleDelete,
-    setRecord,
+  imagenesDetails,
+  handleAdd,
+  handleDelete,
+  agregarImagen,
 }) {
-    return (
-        <>
-        {imagenesDetails.map((val, idx) => {
-            let url= `url- ${idx}`;
-            let descripcion= `descripcion-${idx}`;
-            /* setRecord(val); */
+ // TODO: Crear componente imagen y mover esto para allá
+  const [imagenDetails, setImagenDetails] = useState({
+    url: "",
+    descripcion: "",
+  });
 
-            return(
-                <div className={styles.fila}>
-                    <EstandarInput
-                        /* onChangeItem={propertyName}
-                        setOnChangeItem={setPropertyName} */
-                        name="url"
-                        placeholder="Insertar https://"
-                        id={url}
-                        dataId={idx}
-                    />
-                    <DropInput
-                        /* setOnChangeItem={setDescripcionImagen}
-                        onChangeItem={descripcionImagen} */
-                        name="descripcion"
-                        placeholder="Descripción"
-                        id={descripcion}
-                        dataId={idx}
-                    />
-                    <div className={styles.containerButton}>
-                        {idx === 0 ? (
-                        <button className={styles.add} onClick={handleAdd}><i className="fas fa-plus"></i></button>
-                        ):(
-                        <button className={styles.delete} onClick={() => handleDelete(val)} ><i className="fas fa-times"></i></button>
-                        )}
-                    </div>
+  const setUrl = (url) =>
+    setImagenDetails({
+      url: url,
+      descripcion: imagenDetails.descripcion,
+    });
+
+  const setDescripcion = (descripcion) =>
+    setImagenDetails({
+      url: imagenDetails.url,
+      descripcion: descripcion,
+    });
+
+  return (
+    <>
+      {imagenesDetails.map((val, idx) => (
+        <Imagen
+          value={val}
+          index={idx}
+          agregarImagen={agregarImagen}
+          handleAdd={handleAdd}
+          handleDelete={handleDelete}
+        />
+      ))}
+      {/* {imagenesDetails.map((val, idx) => {
+
+        return (
+          <div className={styles.fila}>
+            <EstandarInput
+              setOnChangeItem={setUrl}
+              name="url"
+              placeholder="Insertar https://"
+              id={url}
+              dataId={idx}
+            />
+            <DropInput
+              setOnChangeItem={setDescripcion}
+              name="descripcion"
+              placeholder="Descripción"
+              id={descripcion}
+              dataId={idx}
+            />
+            <div className={styles.containerButton}>
+              {idx === 0 ? (
+                <div className={styles.containerBotones}>
+                  <button
+                    className={styles.add}
+                    onClick={() => {
+                      agregarImagen(imagenDetails);
+                    }}
+                  >
+                    <i className="fas fa-check"></i>
+                  </button>
+                  <button className={styles.add} onClick={handleAdd}>
+                    <i className="fas fa-plus"></i>
+                  </button>
                 </div>
-            )
-        })}
-        </>
-    ) 
+              ) : (
+                <div className={styles.containerBotones}>
+                  <button
+                    className={styles.add}
+                    onClick={() => {
+                      agregarImagen(imagenDetails);
+                    }}
+                  >
+                    <i className="fas fa-check"></i>
+                  </button>
+                  <button
+                    className={styles.delete}
+                    onClick={() => handleDelete(val)}
+                  >
+                    <i className="fas fa-times"></i>
+                  </button>
+                </div>
+              )}
+            </div>
+          </div>
+        );
+      })} */}
+    </>
+  );
 }
