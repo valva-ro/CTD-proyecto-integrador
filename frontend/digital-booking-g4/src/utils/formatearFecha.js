361c123cc82ce4ea1a0169d40059890830de46e2
Valentina Varela Rodríguez <vavarela.rodriguez@gmail.com>
2021-12-08 00:55:59 -0300

    [FIX] replaceAll en vez de replace

--- frontend/digital-booking-g4/src/utils/formatearFecha.js
+++ frontend/digital-booking-g4/src/utils/formatearFecha.js
@@ -1,17 +1,17 @@
 export default function formatearFecha(date) {
-    if (typeof date == "string") {
-        date = new Date(date)
-    }
-    const dateFormat = date
-        .toLocaleDateString("en-GB", {
-            year: "numeric",
-            month: "2-digit",
-            day: "2-digit",
-        })
-        .replace("/", "-")
-        .split("-")
-        .reverse()
-        .join("-")
+  if (typeof date == "string") {
+    date = new Date(date);
+  }
+  const dateFormat = date
+    .toLocaleDateString("en-GB", {
+      year: "numeric",
+      month: "2-digit",
+      day: "2-digit",
+    })
+    .replaceAll("/", "-")
+    .split("-")
+    .reverse()
+    .join("-");
 
-    return dateFormat;
+  return dateFormat;
 }
