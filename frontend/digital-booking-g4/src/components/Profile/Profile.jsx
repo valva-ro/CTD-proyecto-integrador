fb25e4e8d50e8db2fe28da2a15d2a2b6f449adc8
Valentina Varela Rodríguez <vavarela.rodriguez@gmail.com>
2021-12-09 01:22:14 -0300

    [FIX] URL pagina administracion

--- frontend/digital-booking-g4/src/components/Profile/Profile.jsx
+++ frontend/digital-booking-g4/src/components/Profile/Profile.jsx
@@ -64,7 +64,7 @@ export default function Profile() {
       items = (
         <>
           <Link
-            to="/administration"
+            to="/management"
             onClick={toggleDropdown}
             className={styles.itemDropdown}
           >
