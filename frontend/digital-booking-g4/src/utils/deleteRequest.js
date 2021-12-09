d92191debc2b41fda57a4fa31f4c7d948fff9a8e
miguelEspinosa1 <espinosaorganista1@gmail.com>
2021-12-09 01:14:55 -0500

    [FIX] Conexiones url remotas

--- frontend/digital-booking-g4/src/utils/deleteRequest.js
+++ frontend/digital-booking-g4/src/utils/deleteRequest.js
@@ -1,7 +1,9 @@
+import backUrl from "../resources/backUrl"
+
 export default async function deleteRequest(path = "", headers = { "Content-Type": "application/json" }) {
   const settings = {
     method: "DELETE",
     headers,
   };
-  return await fetch(`http://localhost:8080/${path}`, settings);
+  return await fetch(`${backUrl()}${path}`, settings);
 }
